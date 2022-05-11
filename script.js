67318681b46fcd864eb10572e84f294842c158f3
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-05-11 16:24:07 +0200

    Esercizio completato

--- script.js
+++ script.js
@@ -5,9 +5,34 @@ const app = new Vue({
 
   data: {
 
+    loading: null,
+    mailList: "",
+    numberEmail: 10
+
   },
 
   methods: {
+
+    generateRandomMail(){
+
+      this.loading = true
+      this.mailList= ""
+
+      setTimeout(() => {
+        this.loading = !this.loading
+
+        for (let x = 0; x < this.numberEmail; x++) {
+          axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(item => {
+              this.mailList += " " + item.data.response
+              console.log(this.mailList);
+            })
+  
+            this.mailList= "";
+        }
+      }, 3000);
+
+    }
 
   },
 
@@ -16,3 +41,4 @@ const app = new Vue({
   }
 
 })
+
